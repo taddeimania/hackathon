83ecc4f08ab5155a7cc33c0886035b2ea6bf5932
taddeimania <jtaddei@gmail.com>
2014-03-15 10:44:07 -0500

    Tech Debt: bash index route with template

--- reporanker/urls.py
+++ reporanker/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from .views import SearchView
+from .views import SearchView, IndexView
 
 
 urlpatterns = patterns(
     '',
+    url(r'^$', IndexView.as_view(), name='landing-page'),
     url(r'^search/$', SearchView.as_view(), name='search-view'),
 )
--- reporanker/views.py
+++ reporanker/views.py
@@ -1,7 +1,10 @@
-from vanilla import FormView
+from vanilla import FormView, TemplateView
 
 from .forms import SearchForm
 
+
+class IndexView(TemplateView):
+    template_name = 'reporanker/index.html'
 
 class SearchView(FormView):
     form_class = SearchForm
